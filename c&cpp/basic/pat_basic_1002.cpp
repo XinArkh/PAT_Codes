2a2c1e32ac426b642ea695cc15af8bf946c0eca8
Hsin <wuhsin@zju.edu.cn>
2019-03-07 21:49:33 +0800

    cpp basic 1002

---
+++ c&cpp/basic/pat_basic_1002.cpp
@@ -0,0 +1,61 @@
+#include <cstdio>
+
+void pinyin(int n);
+
+int main()
+{
+    char n[100];
+    scanf("%s", n);
+    int sum = 0;
+    for(int i = 0; n[i] != '\0'; i++){
+        sum += n[i] - '0';
+    }
+    int sum_inv[100];
+    int len = 0;
+    do{
+        sum_inv[len++] = sum % 10;
+        sum /= 10;
+    } while(sum > 0);
+    for(int i = len - 1; i >= 0; i--){
+        pinyin(sum_inv[i]);
+        if(i != 0) printf(" ");
+    }
+
+    return 0;
+}
+
+void pinyin(int n)
+{
+    switch(n){
+    case 0:
+        printf("ling");
+        break;
+    case 1:
+        printf("yi");
+        break;
+    case 2:
+        printf("er");
+        break;
+    case 3:
+        printf("san");
+        break;
+    case 4:
+        printf("si");
+        break;
+    case 5:
+        printf("wu");
+        break;
+    case 6:
+        printf("liu");
+        break;
+    case 7:
+        printf("qi");
+        break;
+    case 8:
+        printf("ba");
+        break;
+    case 9:
+        printf("jiu");
+        break;
+    }
+}

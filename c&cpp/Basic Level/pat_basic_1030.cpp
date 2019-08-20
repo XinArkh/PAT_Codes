fc607aa149689b9c3d79ea4d6841f810fea4fbd8
Hsin <wuhsin@zju.edu.cn>
2019-08-20 22:08:56 +0800

    .

---
+++ c&cpp/Basic Level/pat_basic_1030.cpp
@@ -0,0 +1,21 @@
+#include<cstdio>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int N, p;
+    scanf("%d %d", &N, &p);
+    vector<long long> a(N);
+    for(int i=0; i<N; i++){
+        scanf("%lld", &a[i]);
+    }
+    sort(a.begin(), a.end());
+    int ans = 1;
+    for(int i=1; i<N; i++){
+        int j = upper_bound(a.begin()+i+1, a.end(), (long long)a[i]*p) - a.begin();
+        ans = max(ans, j-i);
+    }
+    printf("%d", ans);
+    return 0;
+}

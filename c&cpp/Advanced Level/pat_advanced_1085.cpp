eab642a74c2334c8476cec69d5dccc07ebca753b
Hsin <wuhsin@zju.edu.cn>
2019-08-25 16:54:45 +0800

    .

--- c&cpp/Advanced Level/pat_advanced_1085.cpp
+++ c&cpp/Advanced Level/pat_advanced_1085.cpp
@@ -12,10 +12,10 @@ int main(){
     }
     sort(a.begin(), a.end());
     int ans = 1;
-    for(int i=0; i<N; i++){
-        int j = upper_bound(a.begin()+i+1, a.end(), (long long)a[i]*p) - a.begin();
+    for(int i=0; i<N-1; i++){
+        int j = upper_bound(a.begin()+i+1, a.begin()+N, (long long)a[i]*p) - a.begin();
         ans = max(ans, j-i);
     }
-    printf("%d", ans);
+    printf("%d\n", ans);
     return 0;
 }
